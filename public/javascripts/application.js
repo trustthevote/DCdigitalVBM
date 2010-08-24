178587b7bf717541ba9af1c0b8a2371f67ed5687
Aleksey Gureiev <spyromus@noizeramp.com>
2010-08-24 15:37:59 +0300

    Confirm page beauty

---
+++ public/javascripts/application.js
@@ -0,0 +1,11 @@
+function disableLink(l) {
+  l = $(l);
+  l.addClass('disabled');
+  l.click(function(e) { e.preventDefault(); });
+}
+
+function enableLink(l) {
+  l = $(l);
+  l.removeClass('disabled');
+  l.unbind('click');
+}
