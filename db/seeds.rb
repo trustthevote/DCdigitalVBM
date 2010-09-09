2f1aab49b206c0c87a454672aee1a76ce05c8e6a
Aleksey Gureiev <spyromus@noizeramp.com>
2010-09-09 09:33:56 +0300

    Extra seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,33 @@
 puts "Cleaning the database"
 Precinct.destroy_all
 
-puts "TODO: Enable gemcutter in gemfile back"
-
 puts "Importing seed data"
 path = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures'))
 
 di = DataImport.new
 di.run("#{path}/AddressID.csv", "#{path}/RegSchema.csv")
+
+# -----------------------------------------------------------------------------
+# Sample record with ballot and attestation for testing
+# -----------------------------------------------------------------------------
+
+# Create a precinct
+precinct = Precinct.create!(:name => "Test Precinct")
+
+# Create a split
+split = precinct.splits.create(:name => "Test Split")
+
+# Create a split ballot style
+ballot_style = split.create_ballot_style(:pdf => File.open("#{Rails.root}/db/fixtures/blank_ballot.pdf", "rb"))
+
+# Create a registration
+Registration.create!(
+  :precinct_split_id => split.id,
+  :name              => 'Mike',
+  :pin               => '1234',
+  :zip               => '24001',
+  :voter_id          => '1234',
+  :address           => "142 N Street",
+  :city              => "Washington",
+  :state             => "DC",
+  :attestation       => File.open("#{Rails.root}/db/fixtures/attestation.pdf", "rb"))
