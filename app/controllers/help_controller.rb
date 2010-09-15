0723a3e2f6ffc40c6922212dca042551775ebcdf
Aleksey Gureiev <spyromus@noizeramp.com>
2010-09-15 11:57:49 +0300

    Added absentee voting options

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -4,8 +4,9 @@ class HelpController < ApplicationController
     @section = sanitize(params[:section])
     @page    = sanitize(params[:page])
     
-    if %w( general-questions absentee-voting vote-by-mail-process vote-by-mail ).include?(@section) ||
-       (@section == 'security' && @page != 'index')
+    if %w( general-questions vote-by-mail-process vote-by-mail ).include?(@section) ||
+       (@section == 'security' && @page != 'index') ||
+       (@section == 'absentee-voting' && @page != 'absentee-voting-options')
       render :template => "help/questions_page"
     else
       render :template => "help/#{@section}/#{@page}"
