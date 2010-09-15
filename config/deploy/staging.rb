054880ad5481f3eaf08ffc87d41ce85eda9fc90f
Aleksey Gureiev <spyromus@noizeramp.com>
2010-09-15 09:34:28 +0300

    Added staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,7 +4,8 @@ set :user,      "boeuser"
 set :runner,    user
 set :deploy_to, "/home/#{user}/#{application}"
 set :bundle_path, "/opt/ruby-enterprise-1.8.7-2010.02/bin/bundle"
-
+default_environment["PATH"] = "/bin:/usr/bin:/usr/local/bin:/opt/ruby-enterprise-1.8.7-2010.02/bin"
+  
 role :app, domain
 role :web, domain
 role :db,  domain, :primary => true
