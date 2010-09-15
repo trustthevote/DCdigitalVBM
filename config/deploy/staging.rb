9509b1bb6e18a5440032f138a4cc1e0452d2f9ef
Aleksey Gureiev <spyromus@noizeramp.com>
2010-09-15 20:23:09 +0300

    Removed default path

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,6 @@ set :rails_env, "staging"
 set :user,      "boeuser"
 set :runner,    user
 set :deploy_to, "/home/#{user}/#{application}"
-default_environment["PATH"] = "/bin:/usr/bin:/usr/local/bin:/opt/ruby-enterprise-1.8.7-2010.02/bin"
   
 role :app, domain
 role :web, domain
