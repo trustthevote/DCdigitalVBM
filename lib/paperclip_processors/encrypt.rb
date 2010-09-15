6b2d428c6a91440fd90ff5cd4569a9838f03f3f3
Aleksey Gureiev <spyromus@noizeramp.com>
2010-09-15 11:20:57 +0300

    Better error handling for encryption

--- lib/paperclip_processors/encrypt.rb
+++ lib/paperclip_processors/encrypt.rb
@@ -17,9 +17,34 @@ module Paperclip
 
       raise PaperclipError, "GPG recipient wasn't set" if @recipient.blank?
       
-      `rm -f "#{File.expand_path(dst.path)}"; gpg -o "#{File.expand_path(dst.path)}" -e -r "#{@recipient}" "#{File.expand_path(src.path)}"`
-      
+      begin
+        run("rm", "-f \"#{File.expand_path(dst.path)}\"")
+        run("gpg", "-o \"#{File.expand_path(dst.path)}\" -e -r \"#{@recipient}\" \"#{File.expand_path(src.path)}\"")
+      rescue PaperclipCommandLineError
+        raise PaperclipError, "couldn't be encrypted. Please try again later."
+      end
+
       dst
     end
+    
+    def run(cmd, params = "", expected_outcodes = 0)
+      command = %Q<#{%Q[#{cmd} #{params}].gsub(/\s+/, " ")}>
+      command = "#{command} 2>&1"
+
+      Paperclip.log(command)
+
+      output = `#{command}`
+      unless [expected_outcodes].flatten.include?($?.exitstatus)
+        Paperclip.log output
+        raise PaperclipCommandLineError, "Error while running #{cmd}"
+      end
+
+      output
+    end
+    
+    def bit_bucket #:nodoc:
+      File.exists?("/dev/null") ? "/dev/null" : "NUL"
+    end
+    
   end
 end
