6f1ada70c4d465eb3ff354f8e06a6c5cb693ee24
Aleksey Gureiev <spyromus@noizeramp.com>
2010-09-22 10:03:28 +0300

    Encrypting session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -20,13 +20,16 @@
 
 # Be sure to restart your server when you modify this file.
 
+ActionController::Base.session_store = EncryptedCookieStore
+
 # Your secret key for verifying cookie session data integrity.
 # If you change this key, all old sessions will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_voter_session',
-  :secret      => '897d485481b6d5a2721505143b73484013a2539bcfec5e8890858b5929b08658fdbc5f34c7f218c4ded11c7739a779a41ccfe14dd74dbc68027693a3e3c40401'
+  :key            => '_voter_session',
+  :secret         => '897d485481b6d5a2721505143b73484013a2539bcfec5e8890858b5929b08658fdbc5f34c7f218c4ded11c7739a779a41ccfe14dd74dbc68027693a3e3c40401',
+  :encryption_key => 'b4521a1c32789bb8921c17380c7178bbd91138385b91efc524d0101e7fcb8f48'
 }
 
 # Use the database for sessions instead of the cookie-based default,
---
+++ vendor/plugins/encrypted_cookie_store/lib/encrypted_cookie_store/constants.rb
@@ -0,0 +1,3 @@
+module EncryptedCookieStoreConstants
+	ENCRYPTION_KEY_SIZE = 32
+end
