facb4f6d3d307f419659f8187bfcf19475062527
Aleksey Gureiev <spyromus@noizeramp.com>
2010-09-27 11:28:57 +0300

    Voter info

--- features/step_definitions/content_check_steps.rb
+++ features/step_definitions/content_check_steps.rb
@@ -9,3 +9,19 @@ end
 Then /^I should see the attestation document link$/ do
   page.should have_xpath(%Q{//a[@href='#{voter_attestation_url(@voter)}']})
 end
+
+Then /^I should see voter address$/ do
+  Then %{I should see "#{@voter.address}"}
+  Then %{I should see "#{@voter.city}"}
+  Then %{I should see "#{@voter.state}"}
+  Then %{I should see "#{@voter.zip}"}
+end
+
+Then /^I should see voter status$/ do
+  status = @voter.voted_digitally? ? "Voted" : "Not yet voted"
+  Then %{I should see "#{status}"}
+end
+
+Then /^I should see the review status$/ do
+  Then %{I should see "#{@voter.status.capitalize}"}
+end
