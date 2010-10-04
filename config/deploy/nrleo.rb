6058308ba3d0f433b7322fe84f3e118a52dc1596
Aleksey Gureiev <spyromus@noizeramp.com>
2010-10-04 14:58:16 +0300

    Changed NRLeo destination

--- config/deploy/nrleo.rb
+++ config/deploy/nrleo.rb
@@ -1,8 +1,8 @@
-set :domain,    "noizeramp.com"
+set :domain,    "75.101.128.188"
 set :rails_env, "staging"
-set :user,      "agureiev"
+set :user,      "ec2-user"
 set :runner,    user
-set :deploy_to, "/home/#{user}/domains/dcdvbm2.noizeramp.com"
+set :deploy_to, "/home/#{user}/dcdvbm2"
 set :branch,    "leo"
   
 role :app, domain
