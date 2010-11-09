b1d516c66b6432b047154aab5f0ac67970673058
Aleksey Gureiev <spyromus@noizeramp.com>
2010-11-09 09:59:09 +0200

    Changed IP of the server

--- config/deploy/nrleo.rb
+++ config/deploy/nrleo.rb
@@ -1,8 +1,8 @@
-set :domain,    "75.101.128.188"
+set :domain,    "174.129.222.54"
 set :rails_env, "staging"
 set :user,      "ec2-user"
 set :runner,    user
-set :deploy_to, "/home/#{user}/dcdvbm2"
+set :deploy_to, "/home/#{user}/dcdvbm"
 set :branch,    "leo"
   
 role :app, domain
